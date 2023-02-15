fe16be9a8174d5e8750068a066625dc01c61fa07
tugberkcanozen <tugberkcanozen@gmail.com>
2023-02-15 14:51:20 +0300

    first commit
    first commit

---
+++ AutoScrollableTabs/AutoScrollableTabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoScrollableTabsApp.swift
+//  AutoScrollableTabs
+//
+//  Created by Tuğberk Can Özen on 15.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct AutoScrollableTabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
